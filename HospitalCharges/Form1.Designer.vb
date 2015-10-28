2c6bb6b3350db2215ba5d45c9e556500342ad8d6
Logan Hampton <losesmc@gmail.com>
2015-10-28 05:22:38 -0700

    Initial commit

---
+++ HospitalCharges/Form1.Designer.vb
@@ -0,0 +1,249 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtPhysicalCharges = New System.Windows.Forms.TextBox()
+        Me.txtLabFees = New System.Windows.Forms.TextBox()
+        Me.txtSurgicalCharges = New System.Windows.Forms.TextBox()
+        Me.txtMedication = New System.Windows.Forms.TextBox()
+        Me.txtStayLength = New System.Windows.Forms.TextBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblError = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.txtPhysicalCharges)
+        Me.GroupBox1.Controls.Add(Me.txtLabFees)
+        Me.GroupBox1.Controls.Add(Me.txtSurgicalCharges)
+        Me.GroupBox1.Controls.Add(Me.txtMedication)
+        Me.GroupBox1.Controls.Add(Me.txtStayLength)
+        Me.GroupBox1.Location = New System.Drawing.Point(24, 8)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(280, 256)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Hospital Stay Information"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(32, 202)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(46, 13)
+        Me.Label6.TabIndex = 9
+        Me.Label6.Text = "Physical"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(32, 162)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(51, 13)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Lab &Fees"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(32, 120)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(87, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "S&urgical Charges"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(32, 80)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(59, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "&Medication"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(32, 32)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(61, 26)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Length of" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "&Stay (Days)"
+        '
+        'txtPhysicalCharges
+        '
+        Me.txtPhysicalCharges.Location = New System.Drawing.Point(130, 198)
+        Me.txtPhysicalCharges.Name = "txtPhysicalCharges"
+        Me.txtPhysicalCharges.Size = New System.Drawing.Size(100, 20)
+        Me.txtPhysicalCharges.TabIndex = 4
+        '
+        'txtLabFees
+        '
+        Me.txtLabFees.Location = New System.Drawing.Point(130, 158)
+        Me.txtLabFees.Name = "txtLabFees"
+        Me.txtLabFees.Size = New System.Drawing.Size(100, 20)
+        Me.txtLabFees.TabIndex = 3
+        '
+        'txtSurgicalCharges
+        '
+        Me.txtSurgicalCharges.Location = New System.Drawing.Point(130, 118)
+        Me.txtSurgicalCharges.Name = "txtSurgicalCharges"
+        Me.txtSurgicalCharges.Size = New System.Drawing.Size(100, 20)
+        Me.txtSurgicalCharges.TabIndex = 2
+        '
+        'txtMedication
+        '
+        Me.txtMedication.Location = New System.Drawing.Point(130, 76)
+        Me.txtMedication.Name = "txtMedication"
+        Me.txtMedication.Size = New System.Drawing.Size(100, 20)
+        Me.txtMedication.TabIndex = 1
+        '
+        'txtStayLength
+        '
+        Me.txtStayLength.Location = New System.Drawing.Point(130, 38)
+        Me.txtStayLength.Name = "txtStayLength"
+        Me.txtStayLength.Size = New System.Drawing.Size(100, 20)
+        Me.txtStayLength.TabIndex = 0
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.lblTotal)
+        Me.GroupBox2.Controls.Add(Me.Label1)
+        Me.GroupBox2.Location = New System.Drawing.Point(24, 288)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(280, 80)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        '
+        'lblTotal
+        '
+        Me.lblTotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblTotal.Location = New System.Drawing.Point(135, 29)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(100, 23)
+        Me.lblTotal.TabIndex = 3
+        Me.lblTotal.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(32, 34)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Total Cost:"
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(4, 376)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(96, 39)
+        Me.btnCalculate.TabIndex = 2
+        Me.btnCalculate.Text = "&Calculate Charges"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(116, 376)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(96, 39)
+        Me.btnClear.TabIndex = 3
+        Me.btnClear.Text = "C&lear Form"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(228, 376)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(96, 39)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblError
+        '
+        Me.lblError.ForeColor = System.Drawing.Color.Red
+        Me.lblError.Location = New System.Drawing.Point(24, 272)
+        Me.lblError.Name = "lblError"
+        Me.lblError.Size = New System.Drawing.Size(280, 16)
+        Me.lblError.TabIndex = 5
+        Me.lblError.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(329, 430)
+        Me.Controls.Add(Me.lblError)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Hospital Charges"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents lblTotal As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtPhysicalCharges As TextBox
+    Friend WithEvents txtLabFees As TextBox
+    Friend WithEvents txtSurgicalCharges As TextBox
+    Friend WithEvents txtMedication As TextBox
+    Friend WithEvents txtStayLength As TextBox
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lblError As Label
+End Class
---
+++ HospitalCharges/Form1.vb
@@ -0,0 +1,59 @@
+﻿Public Class Form1
+    Const intPER_DAY_CHARGE As Integer = 350
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Function CalcStayCharges(intDaysSpent As Integer) As Integer
+        Return intDaysSpent * intPER_DAY_CHARGE
+    End Function
+
+    Function ValidateInputFields(ByRef intStayLength As Integer,
+                                 ByRef decMedication As Decimal,
+                                 ByRef decSurgical As Decimal) As Boolean
+
+        If Not (Integer.TryParse(txtStayLength.Text, intStayLength) And intStayLength > 0) Then
+            PrintError("Length of Stay")
+            Return False
+        End If
+        If Not (Decimal.TryParse(txtMedication.Text, decMedication) And decMedication > 0) Then
+            PrintError("Medication")
+            Return False
+        End If
+        If Not (Decimal.TryParse(txtSurgicalCharges.Text, decSurgical) And decSurgical > 0) Then
+            PrintError("Surgical Charges")
+            Return False
+        End If
+
+        Return True
+    End Function
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim intStayLength As Integer, decMedication As Decimal, decSurgical As Decimal
+
+        lblError.Text = String.Empty
+
+        If ValidateInputFields(intStayLength, decMedication, decSurgical) Then
+            MessageBox.Show("Stay costs: " & intStayLength.ToString("c"))
+            MessageBox.Show("Medication costs: " & decMedication.ToString("c"))
+            MessageBox.Show("Surgical costs: " & decSurgical.ToString("c"))
+        End If
+    End Sub
+
+    Sub PrintError(ByVal strField As String)
+        lblError.Text = "Enter a positive number into " & strField & " field."
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+
+        'Clear the textboxes and error label and set focus back onto stay length field
+        txtStayLength.Text = String.Empty
+        txtMedication.Text = String.Empty
+        txtSurgicalCharges.Text = String.Empty
+        txtLabFees.Text = String.Empty
+        txtPhysicalCharges.Text = String.Empty
+        lblError.Text = String.Empty
+        txtStayLength.Focus()
+    End Sub
+End Class
