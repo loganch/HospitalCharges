e246a6f4cd4ed197ee812c4831bd509d3e4b2230
Logan Hampton <losesmc@gmail.com>
2015-10-29 20:07:01 -0700

    Finish validation checks and remaining calculating functions

--- HospitalCharges/Form1.vb
+++ HospitalCharges/Form1.vb
@@ -5,24 +5,34 @@
         Me.Close()
     End Sub
 
-    Function CalcStayCharges(intDaysSpent As Integer) As Integer
-        Return intDaysSpent * intPER_DAY_CHARGE
+    Function CalcStayCharges(intDaysSpent As Integer) As Decimal
+        Return CDec(intDaysSpent * intPER_DAY_CHARGE)
     End Function
 
     Function ValidateInputFields(ByRef intStayLength As Integer,
                                  ByRef decMedication As Decimal,
-                                 ByRef decSurgical As Decimal) As Boolean
+                                 ByRef decSurgical As Decimal,
+                                 ByRef decLabFees As Decimal,
+                                 ByRef decPhysical As Decimal) As Boolean
 
         If Not (Integer.TryParse(txtStayLength.Text, intStayLength) And intStayLength > 0) Then
             PrintError("Length of Stay")
             Return False
         End If
-        If Not (Decimal.TryParse(txtMedication.Text, decMedication) And decMedication > 0) Then
+        If Not (Decimal.TryParse(txtMedication.Text, decMedication) And decMedication >= 0) Then
             PrintError("Medication")
             Return False
         End If
-        If Not (Decimal.TryParse(txtSurgicalCharges.Text, decSurgical) And decSurgical > 0) Then
+        If Not (Decimal.TryParse(txtSurgicalCharges.Text, decSurgical) And decSurgical >= 0) Then
             PrintError("Surgical Charges")
+            Return False
+        End If
+        If Not (Decimal.TryParse(txtLabFees.Text, decLabFees) And decLabFees >= 0) Then
+            PrintError("Lab Fees")
+            Return False
+        End If
+        If Not (Decimal.TryParse(txtPhysicalCharges.Text, decPhysical) And decPhysical >= 0) Then
+            PrintError("Physical")
             Return False
         End If
 
@@ -30,14 +40,19 @@
     End Function
 
     Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
-        Dim intStayLength As Integer, decMedication As Decimal, decSurgical As Decimal
+        Dim intStayLength As Integer, decMedication As Decimal, decSurgical As Decimal,
+            decLabFees As Decimal, decPhysical As Decimal, decTotal As Decimal, decStayCosts As Decimal,
+            decMiscCosts As Decimal
 
         lblError.Text = String.Empty
 
-        If ValidateInputFields(intStayLength, decMedication, decSurgical) Then
-            MessageBox.Show("Stay costs: " & intStayLength.ToString("c"))
-            MessageBox.Show("Medication costs: " & decMedication.ToString("c"))
-            MessageBox.Show("Surgical costs: " & decSurgical.ToString("c"))
+        If ValidateInputFields(intStayLength, decMedication, decSurgical, decLabFees, decPhysical) Then
+            'After input checks out, get the costs and add them together
+            decStayCosts = CalcStayCharges(intStayLength)
+            decMiscCosts = CalcMiscCharges(decMedication, decSurgical, decLabFees, decPhysical)
+            decTotal = CalcTotalCharges(decStayCosts, decMiscCosts)
+            'Display the total
+            lblTotal.Text = decTotal.ToString("c")
         End If
     End Sub
 
@@ -56,4 +71,15 @@
         lblError.Text = String.Empty
         txtStayLength.Focus()
     End Sub
+
+    Function CalcMiscCharges(ByVal decMedication As Decimal,
+                             ByVal decSurgical As Decimal,
+                             ByVal decLabFees As Decimal,
+                             ByVal decPhysical As Decimal) As Decimal
+        Return decMedication + decSurgical + decLabFees + decPhysical
+    End Function
+
+    Function CalcTotalCharges(decStayCosts As Decimal, decMiscCosts As Decimal) As Decimal
+        Return decStayCosts + decMiscCosts
+    End Function
 End Class
